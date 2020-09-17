b0acb0a74da58cffcbc2dbfe793591e920fa687d
Sergey Novikov <foxinazombobombo@mail.ru>
2020-09-17 09:16:43 +0300

    Adds main project files
    In project have been added main files:
    
    	* index.html;
    
    	* main styles files
    
    	* simonsGame.vue — Vue-component for future app «Simons Game».

--- source/components/simonsGame.vue
+++ source/components/simonsGame.vue
@@ -1,7 +1,39 @@
 <template>
-  <div class="simons-game-wrapper">
-    <h1>{{ simonsGreeting }}</h1>
-  </div>
+  <section class="game">
+    <h2 class="visually-hidden">Simons Game Application</h2>
+
+    <div class="game__tiles tiles">
+      <div class="tiles__group tiles__group--top">
+        <button class="tiles__item tiles__item--top-left"></button>
+        <button class="tiles__item tiles__item--top-right"></button>
+      </div>
+      <div class="tiles__group tiles__group--bottom">
+        <button class="tiles__item tiles__item--bottom-left"></button>
+        <button class="tiles__item tiles__item--bottom-right"></button>
+      </div>
+    </div>
+
+    <div class="game__difficulty">
+      <p class="game__difficulty-heading">Difficulty levels</p>
+
+      <ol class="difficulty-levels">
+        <li class="difficulty-levels__item">
+          <input type="radio" name="difficulty-easy" id="easy" checked>
+          <label for="easy">Easy</label>        
+        </li>
+
+        <li class="difficulty-levels__item">
+          <input type="radio" name="difficulty-medium" id="medium">
+          <label for="medium">Medium</label>        
+        </li>
+
+        <li class="difficulty-levels__item">
+          <input type="radio" name="difficulty-hard" id="hard">
+          <label for="hard">Hard</label>        
+        </li>
+      </ol>
+    </div>
+  </section>
 </template>
 
 
@@ -9,7 +41,7 @@
   module.exports = {
     data() {
       return {
-        simonsGreeting: 'Hi! I am Simon! :)'
+        
       }
     },
     methods: {}
