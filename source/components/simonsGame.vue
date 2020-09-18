528738dd72cadbc20dd88dae243001c0f0e80a24
Sergey Novikov <foxinazombobombo@mail.ru>
2020-09-18 03:40:36 +0300

    Carrieds refactoring of code
    Refactoring of code has been carried.
    
    Comments have also been added.

--- source/components/simonsGame.vue
+++ source/components/simonsGame.vue
@@ -43,17 +43,15 @@
 
 <script>
   module.exports = {
-    data() {
-      return {
-        
-      }
-    },
     methods: {
       onPlayClick () {
         // *** Constants ***
-        var CAN_CLICK = false;
-        var FIRST_ELEMENT = 0;
+        var CAN_CLICK = false; // --- Boolean flag to toggle the ability to click on tiles.
+        var FIRST_ELEMENT = 0; // --- First array element
         var ZERO = 0;
+        var ONE = 1;
+        var BREAK_TIME = 1000;
+        var TIMING_INTERVAL = 300;
 
         // *** DOM-elements ***
         var tiles = document.querySelectorAll('.tiles__item');
@@ -68,25 +66,26 @@
           hard: 400
         };
 
+
         /*
           __________________________________
 
-          ----------- MAIN LOGIC -----------
+          =========== MAIN LOGIC ===========
           __________________________________
 
         */
+
+        /* ====================== Preparatory Settings ====================== */
+        // --- Hiding a loss message  ---
         gameOverMessage.classList.remove('game__loss-message--show');
 
+        // --- Disabling difficulty togglers ---
         Array.from(difficultyTogglers).forEach(element => {
           element.setAttribute('disabled', 'disabled');
         });
 
 
-        var getRandomTileObject = function () {
-          return arrayOfTileObjects[Math.floor(FIRST_ELEMENT + Math.random() * tiles.length)];
-        };
-
-
+        // --- Template for tile object ---
         var getTileObject = function (tile, index) {          
           return {
             name: tile,
@@ -95,6 +94,13 @@
         };
 
 
+        // --- Getting a random tile ---
+        var getRandomTileObject = function () {
+          return arrayOfTileObjects[Math.floor(FIRST_ELEMENT + Math.random() * tiles.length)];
+        };
+
+
+        // --- Play tile sound function ---
         var playSound = function (targetObject) {
           for (var i = 0; i < arrayOfTileObjects.length; i++) {
             if (arrayOfTileObjects[i].name.classList.value === targetObject.classList.value) {
@@ -104,14 +110,16 @@
         };
 
         
+        // --- Populating an array of tile objects with data ---
         Array.from(tiles).forEach((element, index) => {
-          var soundNum = index + 1;
+          var soundNum = index + ONE;
           arrayOfTileObjects.push(getTileObject(element, soundNum));  
         });
 
 
-        var randomTilesSequence = [getRandomTileObject()];
-        var tilesSequenceToGuess = randomTilesSequence.slice();
+        /* ====================== Main Settings ====================== */
+        var randomTilesSequence = [getRandomTileObject()]; // --- Sequence of random tiles
+        var tilesSequenceToGuess = randomTilesSequence.slice(); // --- Sequence of tiles to guess
 
 
         // *** Event Listener for click on the tiles ***
@@ -121,38 +129,61 @@
           if (CAN_CLICK) {
             playSound(evt.target);
 
+            // --- Expected tile is a first tile in sequence to guess ---
             var expectedTile = tilesSequenceToGuess.shift();
 
+
+            /*
+              If expected tile matches the clicked tile,
+              starting new round.
+
+              Else — Game Over.
+            */
             if (expectedTile.name.classList.value === evt.target.classList.value) {
               if (tilesSequenceToGuess.length === ZERO) {
-                // --- Start new round---
+                /*
+                * --- Start new round---
+                */
+
+                // --- Break before starting a new round ---
                 setTimeout(() => {
                   randomTilesSequence.push(getRandomTileObject());
                   tilesSequenceToGuess = randomTilesSequence.slice();
+
                   startFlashing();
-                }, 1000);
+                }, BREAK_TIME);
 
               }
             } else {
+              /*
+              * --- Game Over---
+              */
+
+              // --- Activate of difficulty togglers ---
               Array.from(difficultyTogglers).forEach(element => {
                 element.removeAttribute('disabled');
               });
 
+              // --- Showing the message about the loss ---
               gameOverMessage.classList.add('game__loss-message--show');
 
               randomTilesSequence = [getRandomTileObject()];
               tilesSequenceToGuess = randomTilesSequence.slice();
+
+              // --- Prohibition of clicking on tiles after a loss ---
               CAN_CLICK = false;
             }
           }
         };
 
+
+        // --- Attaching event listeners of click on tiles ---
         tiles.forEach(tile => {
           tile.addEventListener('click', onTileClick);
         });
 
 
-        // *** Function to starting flashing tiles ***
+        // *** Function for Getting Flashing tile ***
         var getFlashingTile = function (tile, tileSound, speed) {
           for (var i = 0; i < difficultyTogglers.length; i++) {
             if (difficultyTogglers[i].checked) {
@@ -165,17 +196,17 @@
             tileSound.play();
 
             setTimeout(() => {
-              console.log('Active tile:', tile);
               tile.classList.toggle('active');
 
               setTimeout(() => {
                 resolve();
-              }, 300);
+              }, TIMING_INTERVAL);
             }, DifficultyLevel[speed]);
           });
         };
 
 
+        // --- Function to starting flashing ---
         var startFlashing = async function () {
           CAN_CLICK = false;
 
@@ -191,10 +222,3 @@
     }
   }
 </script>
-
-
-<style>
-  .active {
-    background-color: red;
-  }
-</style>
